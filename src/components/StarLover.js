1eea01c0f4bf15003634a527545be7e4869ca395
philippe tarte <tarteserviceinformatique@sfr.fr>
2018-05-17 13:09:02 +0200

    add comments on StarLover

--- src/components/StarLover.js
+++ src/components/StarLover.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 import { ListGroupItem } from 'reactstrap'
 
 class StarLover extends Component {
+    // overwrite render method (display)
     render() {
         return <ListGroupItem>StarLover</ListGroupItem>
     }
-
+    // overwrite didmount method (called when component is here)
     componentDidMount() {
-        
+
     }
 }
 export default StarLover
