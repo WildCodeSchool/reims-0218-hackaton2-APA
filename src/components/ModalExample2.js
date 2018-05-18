85425f89d3e43499a5c7af86e20c7915b4d9b37e
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2018-05-18 11:26:40 +0200

    add photo

--- src/components/ModalExample2.js
+++ src/components/ModalExample2.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import forrest from "../images/forrest.gif";
+import thomas from "../images/thomas.jpg";
+import { Row, Col } from "reactstrap"
 
 class ModalExample2 extends React.Component {
   constructor(props) {
@@ -18,12 +20,19 @@ class ModalExample2 extends React.Component {
   }
 
   render() {
-    return (
-      <div className="mt-5">
-        <p className="text-center"><img src={forrest}  style={{width: '1250px', height: '750px'}} alt="rez"/></p>
-        <p size="5" className="text-center"> @TINGALACTIK</p>
+    return <div className="mt-5 container-fluid">
+        <Row>
+          <Col xs="6">
+        <p className="text-center"><img src={forrest}  style={{width: '750px', height: '750px'}} alt="rez"/></p>
+        
+          </Col>
+          <Col xs="6">
+          <p className="text-center"><img src={thomas}  style={{width: '450px', height: '750px'}} alt="rez"/></p>
+          </Col>
+        </Row>
+        <p size="5" className="text-center mt-3"> @TINGALACTIK ET MERCI THOMAS</p>
       </div>
-    );
+    ;
   }
 }
 
